86908127e7d8c81218df76ae24ac0adad7d27e78
Michał Gacek <gacek.michael@outlook.com>
2017-02-25 18:24:21 +0100

    Fixes

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,13 @@
-var a,
-	b,
-	value;
-
-a = prompt('Podaj pierwszą wartość');
-b = prompt('Podaj drugą wartość');
-
-value = (a*a) + (2*a*b) - (b*b);
+var a = prompt('Podaj pierwszą wartość'),
+    b = prompt('Podaj drugą wartość'),
+    value = (a * a) + (2 * a * b) - (b * b);
 	
 console.log(value);
 
-if (value>0) {
+if (value > 0) {
 	console.log('Wynik dodatni');
-} else if (value<0) {
-	console.log('Wynik ujemny')
+} else if (value < 0) {
+	console.log('Wynik ujemny');
 } else {
-	console.log('Wynik równy 0')
+	console.log('Wynik równy 0');
 }
