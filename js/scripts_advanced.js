385f7f0ce812a7d3cf3def4f0987a53d8b0f7281
Marcin Kochanek <md.kochanek@gmail.com>
2017-02-27 23:00:23 +0100

    Adding extended version of exercise 8.7 (uncompleted)

---
+++ js/scripts_advanced.js
@@ -0,0 +1,40 @@
+var maleNames = ['Marcin', 'Adam', 'Maciej', 'Kacper'],
+	femaleNames = ['Ola', 'Kasia', 'Edyta', 'Beata'],
+	allNames = maleNames.concat(femaleNames),
+	newName = prompt('Podaj imię', 'Marian'),
+	indexOfNewName = allNames.indexOf(newName); 
+
+if (newName.length != 0) {
+	if (indexOfNewName === -1) {
+		indexOfNewName = allNames.push(newName);
+		alert('Imię "' + newName + '" zostało dodane do tablicy.');
+		} else {
+			newName = prompt('Imię "' + newName + '" już znajduje się w tablicy. Wybierz inne imię.', 'Marian');
+			indexOfNewName = allNames.indexOf(newName);
+			if (indexOfNewName != -1) {
+				alert('Po raz drugi wybrałeś imię z tablicy. Do widzenia!');
+			} else {
+				indexOfNewName = allNames.push(newName);
+				alert('Imię "' + newName + '" zostało dodane do tablicy.');
+			}
+		}
+} else {
+	newName = prompt('Podane imię musi mieć co najmniej 1 literę. Podaj jeszcze raz.', 'Marian');
+	if (newName.length = 0) {
+		alert('Po raz drugi nie wpisałeś imienia w okienko. Do widzenia!');
+		} else {
+			if (indexOfNewName === -1) {
+				indexOfNewName = allNames.push(newName);
+				alert('Imię "' + newName + '" zostało dodane do tablicy.');
+				} else {
+					newName = prompt('Imię "' + newName + '" już znajduje się w tablicy. Wybierz inne imię.', 'Marian');
+					indexOfNewName = allNames.indexOf(newName);
+					if (indexOfNewName != -1) {
+						alert('Po raz drugi wybrałeś imię z tablicy. Do widzenia!');
+					}
+				}
+			}
+		}
+
+console.log(allNames);
+console.log(newName.length);
